7f6d71a39199429c36d8e5e8910aafd2f73451e6
Shumei Huang <rose1264@gmail.com>
2018-06-14 12:47:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_list = {}
+  data.each do |color_gender_lives_key, color_gender_lives_hash|
+    color_gender_lives_hash.each do |key, names|
+      names.each do |name|
+        new_list[name] ||= {}
+        new_list[name][color_gender_lives_key] ||= []
+        new_list[name][color_gender_lives_key] << key.to_s
+      end
+    end
+  end
+  new_list
 end
