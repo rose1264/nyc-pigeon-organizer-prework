10b1dc09c26dfbc0a58e48905292f5bd13a76340
Shumei Huang <rose1264@gmail.com>
2018-06-14 12:46:13 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,12 @@
 def nyc_pigeon_organizer(data)
   new_list = {}
   data.each do |color_gender_lives_key, color_gender_lives_hash|
-    color_gender_lives_hash.each do |key, name|
-      
+    color_gender_lives_hash.each do |key, names|
+      names.each do |name|
+        new_list[name] ||= {}
+        new_list[name][color_gender_lives_key] ||= []
+        new_list[name][color_gender_lives_key] << key
+      end
     end
   end
   new_list
